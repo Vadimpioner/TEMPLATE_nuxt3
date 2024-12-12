7ba781d9a6670513e783489b334221c06ac783c0
Vadim <pionerbssr@mail.ru>
2024-12-12 23:11:50 +0200

    minore update

--- components/UI/UiButton.vue
+++ components/UI/UiButton.vue
@@ -68,7 +68,7 @@
     }
     &__one {
       background-color: $blue;
-      &:focus-within {
+      &:focus-visible {
         box-shadow: 0px 5px 20px rgba($blue, .6);
       }
       @include hover {
@@ -77,7 +77,7 @@
     }
     &__two {
       background-color: $green;
-      &:focus-within {
+      &:focus-visible {
         box-shadow: 0px 5px 20px rgba($blue, .4);
       }
       @include hover {
